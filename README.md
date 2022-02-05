06f9568075ef1a329e76a8d21d1d351f89b17047
Pieter Pareit <pieter.pareit@gmail.com>
2022-02-05 17:27:12 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,87 @@
+MMIX Mode
+===========
+
+[Emacs](https://www.gnu.org/software/emacs/) major mode for editing
+files in the MMIXAL assembly language. This language is described in
+*The Art of Computer Programming, Volume 1, MMIX A RISC Computer for
+the New Millennium, Fascicle 1* By *Donald E. Knuth*.
+
+Installing
+============
+
+I'll assume a manual installation for now. If this package becomes
+available in MELPA, let me know, I'll update the installation notes.
+I advice you to create a directory in your `.emacs.d` configuration,
+and then clone this project there.
+
+``` bash
+mkdir ~/.emacs.d/lisp-gits/
+cd ~/.emacs.d/lisp-gits/
+git clone git@github.com:ppareit/mmix-mode.git
+```
+
+Dependencies
+---------------
+
+This mode depends on the MMIX binaries:
+
+* [MMIX Home Page](http://mmix.cs.hm.edu/)
+* [Linux Binaries](http://mmix.cs.hm.edu/bin/index.html)
+* [Windows Executables](http://mmix.cs.hm.edu/exe/index.html)
+* [MAC OSX Executables](http://mmix.cs.hm.edu/osx/index.html)
+
+Setting up mmix-mode with use-package
+---------------------------------------------
+
+``` emacs-lisp
+(use-package mmix-mode
+  :load-path "/home/ppareit/.emacs.d/lisp-gits/mmix-mode/")
+```
+Evaluate with `C-x C-e` or restart Emacs.
+
+Using `mmix-mode`
+==============================
+
+* opening a `.mms` file will start this mode.
+
+* `C-j` (`electric-newline-and-maybe-indent`)
+
+This key will move to a new line and insert a tab.
+
+* `C-c C-c` (`compile`)
+  
+  This command compiles the current MMIX program.  The output file
+  is in the same directory and has the extension `.mmo`.
+
+<!-- * `` C-x ` `` (`next-error`) -->
+
+<!--   TBD -->
+
+* `C-c C-r` (`mmix-run`)
+  
+  This command runs the current MMIX program. This command assumes
+  that the program has already been compiled.
+
+
+<!-- Customizing -->
+<!-- ============= -->
+
+<!-- TBD -->
+
+Support
+========
+
+* Issues, requests and questions can go on its [issue tracker](https://github.com/ppareit/mmix-mode/issues).
+* This mode is maintained at its [github page](https://github.com/ppareit/mmix-mode)
+* You can support the maintainer through a [paypal donation](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZBVLYKWYMXQ3G)
+
+Credits
+========
+
+`mmix-mode` was written by:
+
+* Pieter Pareit <pieter.pareit@gmail.com>
+
+The source code is maintained on GitHub at
+<https://github.com/ppareit/mmix-mode> by Pieter Pareit
+(<pieter.pareit@gmail.com>).
---
+++ mmix-mode.el
@@ -0,0 +1,194 @@
+;;; mmix-mode.el --- Major mode for MMIX assambly code
+
+;; Copyright (C) 2022 - 2022 Pieter Pareit <pieter.pareit@gmail.com>
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of
+;; the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;; PURPOSE.  See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public
+;; License along with this program; if not, write to the Free
+;; Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+;; MA 02111-1307 USA
+
+;; Authors: Pieter Pareit <pieter.pareit@gmail.com>
+;; Maintainer: Pieter Pareit <pieter.pareit@gmail.com>
+;; Homepage: https://github.com/ppareit/mmix-mode/
+;; Created: 5 Februari 2022
+;; Last modified: 5 Februari 2022
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "27.2"))
+;; Keywords: mode mmix mmixal mix mixal knuth asm risc
+
+;;; Commentary:
+;; Use this mode for editing files in the MMIXAL language
+;; as described in
+;; The Art of Computer Programming, Volume 1
+;; MMIX A RISC Computer for the New Millennium
+;; Fascicle 1
+;; By Donald E. Knuth
+
+;;; Code:
+
+(defgroup mmix-mode nil
+  "Options for `mmix-mode'."
+  :group 'languages)
+
+(defvar mmix-mode-syntax-table nil
+  "Syntax table for `mmix-mode'.")
+
+(unless mmix-mode-syntax-table
+  (setq mmix-mode-syntax-table (make-syntax-table))
+  (modify-syntax-entry ?# "w")
+  )
+
+(defvar mmix-mode-abbrev-table nil
+  "Abbrev table used while in `mmix-mode'.")
+(define-abbrev-table 'mmix-mode-abbrev-table ())
+
+(defvar mmix-mode-map nil
+  "Keymap used in `mmix-mode' buffers.")
+
+(unless mmix-mode-map
+  (setq mmix-mode-map (make-sparse-keymap))
+  (define-key mmix-mode-map "\C-c\C-c" 'compile)
+  (define-key mmix-mode-map "\C-c\C-r" 'mmix-run)
+  )
+
+;(makunbound 'mmix-mode-map)
+
+(eval-and-compile
+  (defconst mmix-pseudo-ops
+    '("IS" "LOC" "PREFIX" "GREG" "LOCAL" "BSPEC" "ESPEC"
+      "BYTE" "WYDE" "TETRA" "OCTA")
+    "Pseudo-operations of `mmix-mode'.
+Pseudo-operations are operators of MMIXAL but not of MMIX.
+They provide special information about a symbolic program,
+without being instructions of the program itself."))
+
+(eval-and-compile
+  (defconst mmix-ops
+    '("TRAP" "FCMP" "FUN" "FEQL" "FADD" "FIX" "FSUB" "FIXU"
+      "FLOT" "FLOTU" "SFLOT" "SFLOTU"
+      "FMUL" "FCMPE" "FUNE" "FEQLE" "FDIV" "FSQRT" "FREM" "FINT"
+      "MUL" "MULU" "DIV" "DIVU"
+      "ADD" "ADDU" "SUB" "SUBU"
+      "2ADDU" "4ADDU" "8ADDU" "16ADDU"
+      "CMP" "CMPU" "NEG" "NEGU"
+      "SL" "SLU" "SR" "SRU"
+      "BN" "BZ" "BP" "BOD"
+      "BNN" "BNZ" "BNP" "BEV"
+      "PBN" "PBZ" "PBP" "PBOD"
+      "PBNN" "PBNZ" "PBNP" "PBEV"
+      "CSN" "CSZ" "CSP" "CSOD"
+      "CSNN" "CSNZ" "CSNP" "CSEV"
+      "ZSN" "ZSZ" "ZSP" "ZSOD"
+      "ZSNN" "ZSNZ" "ZSNP" "ZSEV"
+      "LDB" "LDBU" "LDW" "LDWU"
+      "LDT" "LDTU" "LDO" "LDOU"
+      "LDSF" "LDHT" "CSWAP" "LDUNC"
+      "LDVTS" "PRELD" "PREGO" "GO"
+      "STB" "STBU" "STW" "STWU"
+      "STT" "STTU" "STO" "STOU"
+      "STSF" "STHT" "STCO" "STUNC"
+      "SYNCD" "PREST" "SYNCID" "PUSHGO"
+      "OR" "ORN" "NOR" "XOR"
+      "AND" "ANDN" "NAND" "NXOR"
+      "BDIF" "WDIF" "TDIF" "ODIF"
+      "MUX" "SADD" "MOR" "MXOR"
+      "SETH" "SETMH" "SETML" "SETL" "INCH" "INCMH" "INCML" "INCL"
+      "ORH" "ORMH" "ORML" "ORL" "ANDNH" "ANDNMH" "ANDNML" "ANDNL"
+      "JMP" "PUSHJ" "GETA" "PUT"
+      "POP" "RESUME" "UNSAVE" "SAVE" "SYNC" "SWYM" "GET" "TRIP")
+    "Operation codes of `mmix-mode'.
+Operation codes of MMIX, short the op codes."))
+
+(eval-and-compile
+  (defconst mmix-alias-ops
+    '("SET" "LDA")
+    "Alias operation codes of `mmix-mode'.
+Alias operations are alternate names for MMIX operations whose standard names
+are inappropriate in certain contexts. They are handled in the same way as the
+`mmix-ops'."))
+
+(eval-and-compile
+  (defconst mmix-globals
+    '("ROUND_CURRENT" "ROUND_OFF" "ROUND_UP" "ROUND_DOWN" "ROUND_NEAR"
+      "Inf"
+      "Data_Segment" "Pool_Segment" "Stack_Segment"
+      "D_BIT" "V_BIT" "W_BIT" "I_BIT" "O_BIT" "U_BIT" "Z_BIT" "X_BIT"
+      "D_Handler" "V_Handler" "W_Handler" "I_Handler"
+      "O_Handler" "U_Handler" "Z_Handler" "X_Handler"
+      "StdIn" "StdOut" "StdErr"
+      "TextRead" "TextWrite" "BinaryRead"
+      "BinaryWrite" "BinaryReadWrite"
+      "Halt"
+      "Fopen" "Fclose" "Fread" "Fgets" "Fgetws" "Fwrite" "Fputs" "Fputws" "Fseek" "Ftell")
+    "Global variables of `mmix-mode'."))
+
+(eval-and-compile
+  (defconst mmix-functions
+    '("Main")
+    "Entry point of MMIX program in `mmix-mode'."))
+
+;; Keywords for Syntax-Highlighting
+(defconst mmix-font-lock-keywords
+  `(("%.*\\|\*.*\\|?.*" . 'font-lock-comment-face)
+    (,(regexp-opt mmix-pseudo-ops 'words) . 'font-lock-preprocessor-face)
+    (,(regexp-opt mmix-alias-ops 'words) . 'font-lock-builtin-face)
+    (,(regexp-opt mmix-ops 'words) . 'font-lock-builtin-face)
+    (,(regexp-opt mmix-globals 'words) . 'font-lock-constant-face)
+    (,(regexp-opt mmix-functions 'words) . 'font-lock-function-name-face)
+   )
+  "Additional expressions to highlight in MMIX mode.")
+
+(defun mmix-indent-line ()
+  "Indent current line as MMIXAL code."
+  (interactive)
+  (insert "\t")
+  )
+
+(defun mmix-compile-command ()
+  "Create a compile command for this buffer."
+  (concat "./mmixal "
+	  (if buffer-file-name (shell-quote-argument buffer-file-name))))
+
+(defun mmix-object-file-name (f-name)
+  "Return the filename of the MMIX object, using F-NAME."
+  (concat (file-name-sans-extension f-name) ".mmo"))
+
+(defun mmix-run ()
+  "Run the mmix program in current buffer.
+This assumes that the file has already been compiled."
+  (interactive)
+  (let* ((object-file-name (mmix-object-file-name buffer-file-name))
+	 (cmd (format "./mmix %s" object-file-name)))
+    (shell-command cmd)))
+
+;;;###autoload
+(define-derived-mode mmix-mode  prog-mode  "MMIX"
+  "Major mode for editing MMIX assembly programs."
+  :group 'mmix-mode
+  :syntax-table nil ;mmix-mode-syntax-table
+  :abbrev-table mmix-mode-abbrev-table
+  :keymap mmix-mode-map
+  (setq-local comment-start "% ")
+  (setq-local font-lock-defaults '(mmix-font-lock-keywords))
+  (setq-local comment-column 32)
+  (setq-local fill-prefix "\t")
+  (setq-local compile-command (mmix-compile-command))
+  (setq-local indent-line-function #'mmix-indent-line)
+  )
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.mms" . mmix-mode))
+
+(provide 'mmix-mode)
+
+;;; mmix-mode.el ends here
